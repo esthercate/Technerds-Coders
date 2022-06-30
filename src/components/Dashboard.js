89ac6616e17f37ea8e32fe2bebd070ab37ae0e86
catherine <vuthi.esther@gmail.com>
2022-06-30 15:05:47 +0300

    catch error method

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,27 @@
 import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  const [myContests, setMyContests] = useState([]);
 
-    const [myContests, setMyContests] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9292/contests")
+      .then((res) => res.json())
+      .then((contestsData) => {
+        setMyContests(contestsData);
+        console.log(contestsData);
+      });
+  }, []);
 
-    useEffect(() => {
-      fetch("http://localhost:9292/contests")
-        .then((res) => res.json())
-        .then((contestsData) => 
-        {setMyContests(contestsData)
-        console.log(contestsData)});
-    }, []);
-
-    function handleDelete(id){
-        fetch(`http://localhost:9292/contests/${id}`, {
-            method: "DELETE",
-        })
-        .then((r) => r.json())
-        .then((deletedContest) => deletedContest)
-    }
+  function handleDelete(id) {
+    fetch(`http://localhost:9292/contests/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then((deletedContest) => deletedContest)
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
   return (
     <div className="h-screen p-6 md:flex">
@@ -52,7 +55,6 @@ const Dashboard = () => {
               </span>{" "}
             </p>
             <p className="m-2">
-              {" "}
               Website:
               <span className="opacity-80 ml-12">
                 {contest.hosting_website}
@@ -63,7 +65,12 @@ const Dashboard = () => {
           </div>
           <div className="card-footer p-5 m-2">
             <button className="w-1/3 p-3 mr-5 cursor-pointer">Edit</button>
-            <button onClick={()=>handleDelete(contest.id)} className="w-1/3 p-3 cursor-pointer">Delete</button>
+            <button
+              onClick={() => handleDelete(contest.id)}
+              className="w-1/3 p-3 cursor-pointer"
+            >
+              Delete
+            </button>
           </div>
         </div>
       ))}
